45e8c5a2562493432360058c82ed5968c275f9fe
indrajithn379 <indrajithn379@gmail.com>
2023-10-08 11:09:15 +0530

    Updates

---
+++ precache-manifest.c5769b9b2f547908fe30a8d6df98d9a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "847e028f3e6d5f4de467880bbb4d5cdf",
+    "url": "/Icecream/index.html"
+  },
+  {
+    "revision": "08fa33825a5f9b66b3b1",
+    "url": "/Icecream/static/css/main.5bc5dad6.chunk.css"
+  },
+  {
+    "revision": "daca279bacb4033db981",
+    "url": "/Icecream/static/js/2.ccdd9737.chunk.js"
+  },
+  {
+    "revision": "15a50c1c2b2805fa91df",
+    "url": "/Icecream/static/js/3.e92f44bb.chunk.js"
+  },
+  {
+    "revision": "08fa33825a5f9b66b3b1",
+    "url": "/Icecream/static/js/main.2b98432e.chunk.js"
+  },
+  {
+    "revision": "595a7df76c921180d00e",
+    "url": "/Icecream/static/js/runtime~main.2ce48dde.js"
+  }
+]);
